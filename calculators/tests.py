1d799747a196dd6614f82d5242ff56fa22eda83d
Aiman <aiman@cprosolutions.com.my>
2022-06-07 18:16:26 +0200

    added a few test cases

--- calculators/tests.py
+++ calculators/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+class CalculatorTests(TestCase):
+
+    def test_calculator_correct_params(self):
+        response = self.client.get('/calculators/v1/qcalculus?uery=MiAqICgyMy8oMyozKSktIDIzICogKDIqMyk=')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['error'], False)
+        self.assertEqual(response.data['result'], '-132.889')
+
+    def test_calculator_wrong_params(self):
+        response = self.client.get('/calculators/v1/qcalculus?uery=12345')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data['error'], True)
+        self.assertEqual(response.data['message'], 'Invalid request')
+    
